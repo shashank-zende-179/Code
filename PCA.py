38f63f4fb484cbe3ce31d7633068ac858aed9c8a
shashank Zende <shashankzende179@gmail.com>
2025-07-17 22:55:09 +0530

    Add files via upload

---
+++ PCA.py
@@ -0,0 +1,44 @@
+from sklearn.datasets import load_iris
+
+iris = load_iris(as_frame=True)
+print(iris.keys())
+
+import matplotlib.pyplot as plt
+
+
+import mpl_toolkits.mplot3d  # noqa: F401
+
+from sklearn.decomposition import PCA
+
+fig = plt.figure(1, figsize=(8, 6))
+ax = fig.add_subplot(111, projection="3d", elev=-150, azim=110)
+
+X_reduced = PCA(n_components=3).fit_transform(iris.data)
+scatter = ax.scatter(
+    X_reduced[:, 0],
+    X_reduced[:, 1],
+    X_reduced[:, 2],
+    c=iris.target,
+    s=40,
+)
+
+ax.set(
+    title="First three PCA dimensions",
+    xlabel="1st Eigenvector",
+    ylabel="2nd Eigenvector",
+    zlabel="3rd Eigenvector",
+)
+ax.xaxis.set_ticklabels([])
+ax.yaxis.set_ticklabels([])
+ax.zaxis.set_ticklabels([])
+
+# Add a legend
+legend1 = ax.legend(
+    scatter.legend_elements()[0],
+    iris.target_names.tolist(),
+    loc="upper right",
+    title="Classes",
+)
+ax.add_artist(legend1)
+
+plt.show()
